040d5a5d9c9472727113ad6edd2bda6af70f2e64
svyakhir <sergeivyahirev@gmail.com>
2025-07-16 15:51:59 +0300

    add function connecting to servers and function executing command on the server to main.py

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+import paramiko
+import time
+from config import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def connect_to_hosts(host): #  Подключение к нодам
+    try:
+        # key = paramiko.RSAKey.from_private_key_file(path_pkey) #  Указываем что вместо пароля используем ключ и путь к приватному ключу
+        ssh = paramiko.client.SSHClient() #  Создание ssh клиента
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # Параметры удаленного сервера. Если подключение по паролю то вместо pkey=key вставить password=password
+        ssh.connect(host, port=port, username=username, password=password)
+        return ssh
+    except Exception:
+        return None
 
+def check_ssh_connect(hosts): # Проверка подключения
+    print(f"Check connecting to hosts...")
+    for host in hosts:
+        connect = connect_to_hosts(host)
+        if connect:
+            print(f"\033[42mConnect to {host} is succesful!\033[0m")
+        else:
+            print(f"\033[41mError connecting to {host}\033[0m")
+            return False
+        connect.close()
+    return True
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def execute_sudo_command(function_connect, command): #  Выполнения комманд под sudo
+    # Подключаемся к root сессии и выполняем команды по root
+    stdin, stdout, stderr = function_connect.exec_command(f"sudo {command}")
+    # stdin.write(f'{password}\n') #  Используем если в /etc/sudoers не отключена проверка пароля
+    # stdin.flush() #  Используем если в /etc/sudoers не отключена проверка пароля
 
+    # Построчно читаем вывод в реальном времени. Если вывода слишком много можно просто закомментить
+    while not stdout.channel.exit_status_ready():
+        if stdout.channel.recv_ready():
+            output = stdout.channel.recv(1024).decode('utf-8')
+            print(f"OUTPUT: {output}", end='')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Вывод результата выполнения команд
+    output = stdout.read().decode()
+    error = stderr.read().decode()
+    if output:
+        print(f"OUTPUT: {output}")
+    if error:
+        print(f"ERROR: {error}")
+    return output
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def sftp_copy(host, local_path, remote_path):  # копирование на ноды указанных в переменных файлов
+    try:
+        # Устанавливаем SSH-соединение
+        key = paramiko.RSAKey.from_private_key_file(path_pkey)
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, username=username, pkey=key)
+
+        # Используем SFTP для передачи файла
+        sftp = ssh.open_sftp()
+        sftp.put(local_path, remote_path)
+    except Exception as error:
+        print(f"Error connecting to {host}: {error}")
+    finally:
+        sftp.close()
+
+def docker_ps(function_coonect):
+    commands = [
+        'docker ps | grep front'
+    ]
+
+    for command in commands:
+        print(f"Executing as sudo {command}")
+        execute_sudo_command(function_coonect, command)
+        time.sleep(2)
+
+if check_ssh_connect(hip_hosting):
+    for host in hip_hosting:
+        print(f"\033[44m Check host {host}\033[0m")
+        connect = connect_to_hosts(host)
+        docker_ps(connect)
+    connect.close()
